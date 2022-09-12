68e09afa1ba8ba03721030a453832a410b88d946
arccc <c23zxcvbnm@163.com>
2022-09-12 13:09:39 +0800

    提交配置

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import Router from './router'
 import ReactDOM from 'react-dom/client'
-import './assets/base.css'
+import './assets/base.less'
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(<Router/>)
